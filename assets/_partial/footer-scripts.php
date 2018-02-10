3d7091129434726a730e19c3ce271e2406f26fd8
adarsh anchan <anchanadarsh@gmail.com>
2018-02-10 20:38:35 +0530

    cab

---
+++ assets/_partial/footer-scripts.php
@@ -0,0 +1,15 @@
+<!-- Online -->
+<!--Taken from jQuery CDN-->
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+<!--Taken from Bootstrap CDN-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+<!-- Offline 
+Downloaded from jquery 
+<script src="js/jquery-1.12.4.min.js"></script>
+Downloaded from get bootstrap
+<script src="js/bootstrap.min.js"></script> -->
+
+<!--Your custom script code-->
+<!--<script src="js/main.js"></script>-->
---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <title>Webpage Title</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <?php include 'assets/_partial/head.php' ?>
+    <!--        <link href="style.css" rel="stylesheet">-->
+</head>
+
+<body ng-app="cowsandbulls">
+    <div class="clearfix"></div>
+    <section ng-controller="cabController" id="section-home1" class="container-fluid text-center p-tb-md bg-f1f" style="height:100vh">
+
+        <button class="btn btn-primary" ng-click="getNum()" ng-hide="showyes">Get 3 digit Number</button>
+        <button class="btn btn-success ng-hide" ng-show="showyes">Number Generated</button>
+
+        <form name="formguessnum" class="m-t-lg">
+            <label>Enter your 3 digit number</label>
+            <input type="text" name="guessnumber" ng-model="guessNumber">
+            <button type="submit" ng-click="numtoarray()">Enter</button>
+            <p class="m-tb-sm" style="color:red" ng-if="repeatedyes">Enter Non-Repeated 3 digit Number only</p>
+        </form>
+
+        <table class="table table-bordered" style="width:50%;margin:30px auto">
+            <tr style="background:#d6d6d6">
+                <th>Guess#</th>
+                <th>Your Number</th>
+                <th>Result</th>
+            </tr>
+            <tr ng-repeat="usernum in userNumArray">
+                <td>{{$index + 1}}</td>
+                <td>{{usernum.guessnum}}</td>
+                <td>{{usernum.res}}</td>
+            </tr>
+        </table>
+
+    </section>
+    <div class="clearfix"></div>
+
+    <?php include 'assets/_partial/footer-scripts.php' ?>
+    <script src="main.js" type="text/javascript"></script>
+
+</body>
+
+</html>
